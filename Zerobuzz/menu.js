b8f3ee85d712b603066c8e0f10f7c7bda42cdff8
SwishNdish <160113248+SwishNdish@users.noreply.github.com>
2024-06-05 18:23:22 +0200

    Delete Zerobuzz directory

--- Zerobuzz/menu.js
+++
@@ -1,8 +0,0 @@
-document.getElementById("menuToggle").addEventListener("click", function () {
-    var menu = document.getElementById("menu");
-    if (menu.style.display === "block") {
-      menu.style.display = "none";
-    } else {
-      menu.style.display = "block";
-    }
-  });
